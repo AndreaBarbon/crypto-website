148e5a3df7de1e0cdd9d8d216144d235febe2bc0
AndreaBarbon <andrea.3arbon@gmail.com>
2017-11-08 01:14:24 +0100

    positions

--- app/models/trade.rb
+++ app/models/trade.rb
@@ -2,4 +2,8 @@
 class Trade < ApplicationRecord
   self.primary_key = "index"
   default_scope { order('date_buy DESC') }
+
+  def crypto_compare_link
+  	"https://www.cryptocompare.com/coins/#{self.ticker}/overview/BTC"
+  end
 end
