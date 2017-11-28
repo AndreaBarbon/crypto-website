8c96d0f0c53be3dd1b3e32d07747bdc3134f3566
AndreaBarbon <andrea.3arbon@gmail.com>
2017-11-28 01:33:34 +0100

    New strategy

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,7 +3,7 @@ class WelcomeController < ApplicationController
     @usd_ret  = File.readlines("/2.0/logs/update_balance.txt")[-8]
     @eur_ret  = File.readlines("/2.0/logs/update_balance.txt")[-7]
     @btc_ret  = File.readlines("/2.0/logs/update_balance.txt")[-6]
-    @data_BTC = File.read("/BTC_chart.csv")
-    @data_USD = File.read("/USD_chart.csv")
+    @data_BTC = File.read("/2.0/BTC_chart.csv")
+    @data_USD = File.read("/2.0/USD_chart.csv")
   end
 end
